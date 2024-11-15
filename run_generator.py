1cb38bca3a7bfbd0c28472e1f64fd24a8d8a2dfe
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-11-15 16:09:02 +0100

    refactor: add code

--- run_generator.py
+++ run_generator.py
@@ -4,14 +4,17 @@ from html_article_generator.article_generator import ArticleHtmlGenerator
 
 def main():
     parser = argparse.ArgumentParser(description="Generate an HTML article from a template.")
-    parser.add_argument("-path", type=str, help="Name of the output template file", required=True)
+    parser.add_argument("-path", type=str, help="Name of the output template file", required=False)
     args = parser.parse_args()
 
     output_path = args.path
 
-    print(f"Output will be saved to: {output_path}")
+    if output_path:
+        print(f"Output will be saved to: {output_path}")
+        generator = ArticleHtmlGenerator(output_path=output_path)
 
-    generator = ArticleHtmlGenerator(output_path=output_path)
+    else:
+        generator = ArticleHtmlGenerator()
 
     print("Starting the article generation process...")
     print(generator)
