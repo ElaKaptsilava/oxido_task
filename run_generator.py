05bc137762e74ad46af4c56fb8e4b8910e2cd5a3
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-11-15 14:56:14 +0100

    feat: write script logic

---
+++ run_generator.py
@@ -0,0 +1,23 @@
+import argparse
+from html_article_generator.article_generator import ArticleHtmlGenerator
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate an HTML article from a template.")
+    parser.add_argument("-path", type=str, help="Name of the output template file", required=True)
+    args = parser.parse_args()
+
+    output_path = args.name
+
+    print(f"Output will be saved to: {output_path}")
+
+    generator = ArticleHtmlGenerator(output_path=output_path)
+
+    print("Starting the article generation process...")
+    print(generator)
+    generator.process_article()
+    print("HTML article generated successfully!")
+
+
+if __name__ == "__main__":
+    main()
